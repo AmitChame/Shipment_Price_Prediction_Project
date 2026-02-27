709e1097b69074c3cb9265f6a7b0ffcfdc3a3aef
Amit Chame <your-email@example.com>
2026-02-27 10:12:55 +0530

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+
+
+setup(
+    name="shipment",
+    version='0.0.1',
+    author='Amit Chame',
+    author_email='amitchame19@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+)
